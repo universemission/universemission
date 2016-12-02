a3210f74dd392e725cb5a494f6caabb8344c3689
Eric Hartin <ehartin@gmail.com>
2016-12-02 11:26:47 +0200

    Testing sound

--- ui-refactored.py
+++ ui-refactored.py
@@ -4,23 +4,37 @@ from pygame.locals import *
 global print
 global input
 
+def parser(command):
+    global imgfile
+    global image
+    if command == 'caccaa':
+        imgfile = 'bridge-ascii-2.png'
+        image = pygame.image.load(imgfile)
+    elif command == 'ei caccaa':
+        imgfile = 'bridge-ascii.png'
+        image = pygame.image.load(imgfile)    
+        
 win = pygcurse.PygcurseWindow(120, 40)
 win.font = pygame.font.Font('terminus.ttf', 14)
-image = pygame.image.load('bridge-ascii.png')
+imgfile = 'bridge-ascii.png'
+image = pygame.image.load(imgfile)
 pygame.display.set_caption('Universe Mission')
 print = win.pygprint
 input = win.input
 box = pygcurse.PygcurseTextbox(win, (1, 27, 118, 12), fgcolor='black', bgcolor='green', border='basic', wrap=True, marginleft=1, margintop=1, caption='Hello world!')
-inventory = pygcurse.PygcurseTextbox(win, (100, 1, 19, 25), fgcolor='black', bgcolor='red', border='basic', wrap=True, marginleft=1, margintop=1, caption='Inventory')
+sidebox = pygcurse.PygcurseTextbox(win, (100, 1, 19, 25), fgcolor='black', bgcolor='red', border='basic', wrap=True, marginleft=1, margintop=1, caption='Inventory')
 box.text = 'This is the Universe Mission test text box.'
-inventory.text = 'Tools         Mops         Other things'
+sidebox.text = 'Tools         Mops         Other things'
+sound = pygame.mixer.Sound('spacehum.ogg')
+sound.play()
 
 while True:
     win.setscreencolors('green', 'black', clear=True)
     win.surface.blit(image,(10,10))
     box.update()
-    inventory.update()
+    sidebox.update()
     win.cursor = 0, win.height-1
-    letter = input(' >')
-    box.text = str(letter)
-
+    command = input(' >')
+    box.text = str(command)
+    parser(command)
+    pygame.display.update()
