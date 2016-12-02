87cbc56c7705619366bc9a534dbb171d460f8f0c
Eric Hartin <ehartin@gmail.com>
2016-12-02 11:38:10 +0200

    Testing sound

--- ui-refactored.py
+++ ui-refactored.py
@@ -1,9 +1,6 @@
 import pygcurse, pygame, sys
 from pygame.locals import *
 
-global print
-global input
-
 def parser(command):
     global imgfile
     global image
@@ -13,28 +10,46 @@ def parser(command):
     elif command == 'ei caccaa':
         imgfile = 'bridge-ascii.png'
         image = pygame.image.load(imgfile)    
-        
-win = pygcurse.PygcurseWindow(120, 40)
-win.font = pygame.font.Font('terminus.ttf', 14)
-imgfile = 'bridge-ascii.png'
-image = pygame.image.load(imgfile)
-pygame.display.set_caption('Universe Mission')
-print = win.pygprint
-input = win.input
-box = pygcurse.PygcurseTextbox(win, (1, 27, 118, 12), fgcolor='black', bgcolor='green', border='basic', wrap=True, marginleft=1, margintop=1, caption='Hello world!')
-sidebox = pygcurse.PygcurseTextbox(win, (100, 1, 19, 25), fgcolor='black', bgcolor='red', border='basic', wrap=True, marginleft=1, margintop=1, caption='Inventory')
-box.text = 'This is the Universe Mission test text box.'
-sidebox.text = 'Tools         Mops         Other things'
-sound = pygame.mixer.Sound('spacehum.ogg')
-sound.play()
 
-while True:
-    win.setscreencolors('green', 'black', clear=True)
-    win.surface.blit(image,(10,10))
-    box.update()
-    sidebox.update()
-    win.cursor = 0, win.height-1
-    command = input(' >')
-    box.text = str(command)
-    parser(command)
-    pygame.display.update()
+def init_ui():
+    global gamewindow
+    global imgfile
+    global image
+    global box
+    global sidebox
+    global print
+    global input
+    
+    gamewindow = pygcurse.PygcurseWindow(120, 40)
+    gamewindow.font = pygame.font.Font('terminus.ttf', 14)
+    print = gamewindow.pygprint
+    input = gamewindow.input
+    imgfile = 'bridge-ascii.png'
+    image = pygame.image.load(imgfile)
+    pygame.display.set_caption('Universe Mission')    
+    box = pygcurse.PygcurseTextbox(gamewindow, (1, 27, 118, 12), fgcolor='black', bgcolor='green', border='basic', wrap=True, marginleft=1, margintop=1, caption='Hello world!')
+    sidebox = pygcurse.PygcurseTextbox(gamewindow, (100, 1, 19, 25), fgcolor='black', bgcolor='red', border='basic', wrap=True, marginleft=1, margintop=1, caption='Inventory')
+    box.text = 'This is the Universe Mission test text box.'
+    sidebox.text = 'Tools         Mops         Other things'
+    sound = pygame.mixer.Sound('spacehum.ogg')
+    sound.play()
+
+def main():
+    global gamewindow
+    global box
+    global sidebox
+
+    init_ui()
+    
+    while True:
+        gamewindow.setscreencolors('green', 'black', clear=True)
+        gamewindow.surface.blit(image,(10,10))
+        box.update()
+        sidebox.update()
+        gamewindow.cursor = 0, gamewindow.height-1
+        command = input(' >')
+        box.text = str(command)
+        parser(command)
+        pygame.display.update()
+
+if __name__ == "__main__": main()
